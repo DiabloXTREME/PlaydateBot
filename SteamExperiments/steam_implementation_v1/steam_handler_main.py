7cd52190d3273f868c5afa190a2f5515e83c0c41
aryam <thediabloxtreme666@gmail.com>
2025-06-29 16:57:03 +0530

    Added more to steam_handler_main.py: logging and Steam.get_user()

--- SteamExperiments/steam_implementation_v1/steam_handler_main.py
+++ SteamExperiments/steam_implementation_v1/steam_handler_main.py
@@ -1,13 +1,35 @@
+# Imports
+import json
 import requests
 import dotenv
 import os
+import logging
+
+# Setup
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+logger = logging.getLogger()
 
 
 class Steam:
-  
-    def __init__(self, values=None):
+
+    def __init__(self):
         self.values = {}
         dotenv.load_dotenv()
-        self.values["BOT_TOKEN"] = os.getenv("BOT_TOKEN")
+        self.values["STEAM_TOKEN"] = os.getenv("STEAM_TOKEN")
+        self.values["STEAM_ID"] = os.getenv("STEAM_ID")
+        logger.info(f"Initializing. Values = [\n{self.values.values()}\n]\n")
 
-        print("vals", self.values)
+    def get_user(self, user_id: int) -> json:
+        """
+        :param user_id:
+        :return:
+        """
+        return requests.request(method="GET",
+                                url=f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v002/?key={self.values["STEAM_TOKEN"]}&steamids={user_id}").json()
+
+
+# Temp Run Command
+obj = Steam()
+logger.info(
+    obj.get_user(int(os.getenv("STEAM_ID")))
+)
